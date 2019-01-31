145e774aa48d3b747e8cb16c9c6ea94b6f8d4436
Michele Cipolla <cipmiky@gmail.com>
2019-01-31 11:30:20 +0100

    refactor: location of tsconfig-build

--- tools/enviroment.js
+++ tools/enviroment.js
@@ -4,7 +4,7 @@ module.exports = {
     root: path.resolve(__dirname, '..'),
     get buildDir() { return path.join(this.root, 'dist'); },
     get sourceDir() { return path.join(this.root, 'src'); },
-    get tsConfigBuild() { return path.join(this.sourceDir, 'tsconfig-build.json'); },
+    get tsConfigBuild() { return path.join(this.root, 'tsconfig-build.json'); },
     get tsLintConfig() { return path.join(this.root, 'tslint.json'); },
     get lintOutput() { return path.join(this.root, 'tslint-out.txt'); }
 };
