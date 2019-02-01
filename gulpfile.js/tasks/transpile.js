b7447d78c509375a28658b0f1138dc49853c15b0
dmike16 <cipmiky@gmail.com>
2019-02-01 21:24:36 +0100

    build: general executor promise

--- gulpfile.js/tasks/transpile.js
+++ gulpfile.js/tasks/transpile.js
@@ -1,19 +1,10 @@
 const { resolveModuleCli } = require("../../tools/resolveModuleCli");
-const { exec } = require("child_process");
 const log = require("fancy-log");
 const env = require("../../tools/enviroment");
+const exec = require("../../gulpfile.js/cmd-executor").executeCmd;
 
-module.exports = () => () => {
-    log.info("Transpiling project with configuration: [", env.tsConfigBuild, "]")
-    return new Promise((resolve, reject) => {
-        exec(`${resolveModuleCli('typescript', 'tsc')} -p ${env.tsConfigBuild}`, (error, stdout, stderr) => {
-            if (error) {
-                log.error(stdout || stderr);
-                reject(error);
-            } else {
-                log.info('Transpile ended', stdout);
-                resolve(0);
-            }
-        });
-    });
+module.exports = () => async () => {
+    log.info("Transpiling project with configuration: [", env.tsConfigBuild, "]");
+    const cmd = `${resolveModuleCli('typescript', 'tsc')} -p ${env.tsConfigBuild}`;
+    await exec(cmd);
 }
--- gulpfile.js/tasks/tslint.js
+++ gulpfile.js/tasks/tslint.js
@@ -1,19 +1,10 @@
-const { exec } = require("child_process");
-const log = require("fancy-log");
 const { resolveModuleCli } = require("../../tools/resolveModuleCli");
 const env = require("../../tools/enviroment");
+const exec = require("../../gulpfile.js/cmd-executor").executeCmd; 
+const log = require("fancy-log");
 
-module.exports = () => () => {
+module.exports = () => async () => {
     log.info('Linting project with tslint: [', env.tsLintConfig, ']');
-    return new Promise((resolve, reject) => {
-        exec(`${resolveModuleCli('tslint', 'tslint')} -c ${env.tsLintConfig} -p ${env.tsConfigBuild}`, (error, stdout, stderr) => {
-            if (error) {
-                log.error(stdout || stderr);
-                reject(error);
-            } else {
-                log.info('Lint ended', stdout);
-                resolve(0);
-            }
-        });
-    });
+    const cmd = `${resolveModuleCli('tslint', 'tslint')} -c ${env.tsLintConfig} -p ${env.tsConfigBuild}`;
+    await exec(cmd);
 };
