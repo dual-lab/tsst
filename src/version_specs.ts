153f9ad8a6e77bdaa317840c792292de4f544af0
dmike16 <cipmiky@gmail.com>
2019-02-08 23:27:00 +0100

    feat(core): version class

--- src/version_specs.ts
+++ src/version_specs.ts
@@ -1,4 +1,21 @@
+import { Version } from "./version";
+
 describe("Version class", () => {
-    it("Should parse semver string");
-    it("Should compare other semver string");
+    it("Should fail on parse not semver string", () => {
+        expect(() => new Version("1.2_23")).toThrowError();
+    });
+    it("Should parse semver string", () => {
+        const ver = new Version("1.2.3");
+        expect(ver.getMajor).toBe(1);
+        expect(ver.getMinor).toBe(2);
+        expect(ver.getPatch).toBe(3);
+    });
+    it("Should compare other semver string", () => {
+        const ver = new Version("1.2.3");
+        expect(ver.isEqual("1.2.3")).toBe(true);
+        expect(ver.isBelow("1.2.4")).toBe(true);
+        expect(ver.isBelow("1.1.4")).toBe(false);
+        expect(ver.isAbove("1.2.2")).toBe(true);
+        expect(ver.isAbove("1.2.4")).toBe(false);
+    });
 });
